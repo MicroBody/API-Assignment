daadeb030cd75624eba7eb54f8f407109bf9e88d
MicroBody <walther.sasaki@gmail.com>
2024-09-22 20:02:47 +0200

    Slowly giving up on this actually working

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb')
 
 let dbConnection
-let uri = 'mongodb+srv://Tester1:<Test1>@cluster0.ljd5c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
 
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect(uri)
+    MongoClient.connect('mongodb+srv://Tester1:<test1>@cluster0.ljd5c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
       .then((client) => {
         dbConnection = client.db()
         return cb()
